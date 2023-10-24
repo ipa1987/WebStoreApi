2fbadd4cb38a0c8669954fa78e22cab2784c192c
DESKTOP-L57BNHU\Idris <idris.patel1987@outlook.com>
2023-10-24 19:20:47 +0100

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStoreApi.Models;
+
+namespace WebStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<UserDto> listUsers = new List<UserDto>()
+        {
+            new UserDto()
+        {
+                FirstName = "Bill",
+                LastName = "Gates",
+                Email = "bill@microsoft.com",
+                Phone = "+123456789",
+                Address = "New York, USA"
+        }
+    };
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            if (listUsers.Count > 0)
+            {
+                return Ok(listUsers);
+            }
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            if (id >= 0 && id < listUsers.Count)
+            {
+                return Ok(listUsers[id]);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(UserDto user)
+        {
+            // check email is not authorised
+
+            if (user.Email.Equals("user@example.com"))
+            {
+                ModelState.AddModelError("Email", "This email is not authorised");
+                return BadRequest(ModelState);
+            }
+
+
+            listUsers.Add(user);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, UserDto user)
+        {
+            if (user.Email.Equals("user@example.com"))
+            {
+                ModelState.AddModelError("Email", "This email is not authorised");
+                return BadRequest(ModelState);
+            }
+
+
+            if (id >= 0 && id < listUsers.Count)
+            {
+                listUsers[id] = user;
+            }
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            if (id >= 0 && id < listUsers.Count)
+            {
+                listUsers.RemoveAt(id);
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ Models/UserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStoreApi.Models
+{
+    public class UserDto
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please provide Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+        [Required]
+        [MinLength(5, ErrorMessage = "Address should have at least 5 characters")]
+        [MaxLength(1000, ErrorMessage = "Address should not be more than 1000 characters")]
+        public string Address {  get; set; } = string.Empty;
+    }
+}
